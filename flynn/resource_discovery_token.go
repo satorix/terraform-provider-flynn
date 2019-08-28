603fdd616eff305566658be0dcd0da6cce650bf7
Bret Baptist <bbaptist@iexposure.com>
2019-08-28 15:48:48 -0500

    Formatting

--- flynn/resource_discovery_token.go
+++ flynn/resource_discovery_token.go
@@ -1,77 +1,77 @@
 package flynn
 
 import (
-	"fmt"
-	"path"
-	"net/http"
-	"net/url"
-	"runtime"
+    "fmt"
+    "path"
+    "net/http"
+    "net/url"
+    "runtime"
 
-	"github.com/hashicorp/terraform/helper/schema"
+    "github.com/hashicorp/terraform/helper/schema"
 )
 
 func resourceDiscoveryToken() *schema.Resource {
-	return &schema.Resource{
-	    Create: resourceDiscoveryTokenCreate,
-		Read:   resourceDiscoveryTokenRead,
-		Update: resourceDiscoveryTokenUpdate,
-		Delete: resourceDiscoveryTokenDelete,
+    return &schema.Resource{
+        Create: resourceDiscoveryTokenCreate,
+        Read:   resourceDiscoveryTokenRead,
+        Update: resourceDiscoveryTokenUpdate,
+        Delete: resourceDiscoveryTokenDelete,
 
-		Schema: map[string]*schema.Schema{
-			"token": {
-				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"server": {
-				Type:        schema.TypeString,
-				Optional:    true,
-				Default:     "https://discovery.flynn.io",
-				Description: "The server used to retrieve a discovery token\nIf not set, defaults to https://discovery.flynn.io",
-			},
-		},
-	}
+        Schema: map[string]*schema.Schema{
+            "token": {
+                Type:     schema.TypeString,
+                Computed: true,
+            },
+            "server": {
+                Type:        schema.TypeString,
+                Optional:    true,
+                Default:     "https://discovery.flynn.io",
+                Description: "The server used to retrieve a discovery token\nIf not set, defaults to https://discovery.flynn.io",
+            },
+        },
+    }
 }
 
 func getDiscoveryTokenFrom(uri string) (string, error) {
-	req, err := http.NewRequest("POST", uri, nil)
-	if err != nil {
-		return "", err
-	}
-	req.Header.Set("User-Agent", fmt.Sprintf("terraform-provider-flynn/%s-%s", runtime.GOOS, runtime.GOARCH))
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return "", err
-	}
-	if res.StatusCode != http.StatusCreated {
-		return "", urlError("POST", uri, res.StatusCode)
-	}
+    req, err := http.NewRequest("POST", uri, nil)
+    if err != nil {
+        return "", err
+    }
+    req.Header.Set("User-Agent", fmt.Sprintf("terraform-provider-flynn/%s-%s", runtime.GOOS, runtime.GOARCH))
+    res, err := http.DefaultClient.Do(req)
+    if err != nil {
+        return "", err
+    }
+    if res.StatusCode != http.StatusCreated {
+        return "", urlError("POST", uri, res.StatusCode)
+    }
 
-	base, err := url.Parse(uri)
-	if err != nil {
-		return "", err
-	}
-	cluster, err := url.Parse(res.Header.Get("Location"))
-	if err != nil {
-		return "", err
-	}
+    base, err := url.Parse(uri)
+    if err != nil {
+        return "", err
+    }
+    cluster, err := url.Parse(res.Header.Get("Location"))
+    if err != nil {
+        return "", err
+    }
 
-	return base.ResolveReference(cluster).String(), nil
+    return base.ResolveReference(cluster).String(), nil
 }
 
 func resourceDiscoveryTokenCreate(d *schema.ResourceData, meta interface{}) error {
-	server := d.Get("server").(string)
-	uri := server + "/clusters"
+    server := d.Get("server").(string)
+    uri := server + "/clusters"
 
     token, err := getDiscoveryTokenFrom(uri)
 
-	if err == nil {
-		d.Set("token", string(token))
-		d.SetId(path.Base(token))
-	} else {
-		return fmt.Errorf("Error requesting discovery token: %d", err)
-	}
+    if err == nil {
+        d.Set("token", string(token))
+        d.SetId(path.Base(token))
+    } else {
+        return fmt.Errorf("Error requesting discovery token: %d", err)
+    }
 
-	return resourceDiscoveryTokenRead(d, meta)
+    return resourceDiscoveryTokenRead(d, meta)
 }
 
 func resourceDiscoveryTokenRead(d *schema.ResourceData, meta interface{}) error {
@@ -87,10 +87,10 @@ func resourceDiscoveryTokenDelete(d *schema.ResourceData, meta interface{}) erro
 }
 
 func urlError(method, uri string, status int) error {
-	return &url.Error{
-		Op:  method,
-		URL: uri,
-		Err: fmt.Errorf("unexpected status %d", status),
-	}
+    return &url.Error{
+        Op:  method,
+        URL: uri,
+        Err: fmt.Errorf("unexpected status %d", status),
+    }
 
 }
